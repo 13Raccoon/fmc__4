537d61f27388d1356215fe6ece471073a5a3435e
Roman <rr-13@ya.ru>
2021-03-29 16:28:42 +0800

    all

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.img-slaider').slick({
+    /* dots: false,
+    speed: 1200,
+    slidesToShow: 1, */
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.png" alt=""/></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.png" alt=""/></button>',
+    
+  });
+
+  
+});
